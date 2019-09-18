76f82bc90e5ffa0e84ea72b2bc57999174e2ce95
chakrabs <chakrabs@ebRstudio.mskcc.org>
2019-09-18 18:12:34 -0400

    first commit

---
+++ R/bayesecopop-package.R
@@ -0,0 +1,16 @@
+#' The 'bayesecopop' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name bayesecopop-package
+#' @aliases bayesecopop
+#' @useDynLib bayesecopop, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. https://mc-stan.org
+#'
+NULL
